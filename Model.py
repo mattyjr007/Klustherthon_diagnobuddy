cf5c01f9ef66142091053dde28e1d85ab550e81c
Samuel Mathias <mathiasjr007@gmail.com>
2023-11-23 13:20:14 +0100

    models

---
+++ Model.py
@@ -0,0 +1,54 @@
+import os
+from openai import OpenAI
+from dotenv.main import load_dotenv
+
+load_dotenv()
+
+# Set up OpenAI API key
+client = OpenAI(api_key=os.environ['GPT'])
+
+chatshistory = []
+chatshistory.append({
+      "role": "system",
+      "content": "You're a medical assistant chatbot called DiagnoBuddy,\nyour role is to give preliminary medical advice. A user will describe their symptoms, wait and collect input step by step, and you diagnose their possible conditions based on the input, request as much input needed, if need be, for more clarification and proper diagnosis. \nRecommend Home remedies if needed, tell them when they should seek a doctor. help patients decide if they need to seek in-person medical care/Doctors.\nYou will gather information's from the user, request from the user as much information needed to give a diagnosis and medical advice. \nmake the responses short and direct simulate in a human manner. `please strictly adhere to bot objective, do not answer question that is not a medical or health concern.`"
+    })
+
+class Model:
+
+    def __init__(self) -> None:
+        pass
+
+    def chatBot(self,msg:str) -> str:
+
+        # store conversation
+        chatshistory.append({"role": "user","content": f"{msg}"})
+
+        # feed into model
+        response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages= chatshistory,
+            temperature=0.34,
+            max_tokens=350,
+            timeout=300
+        )
+
+        # bot response
+        #bot_response = response.choices[0].message.content
+
+        try:
+            # store bot response
+            bot_response = response.choices[0].message.content
+            
+            # append bot response to conversation
+            chatshistory.append({
+            "role": "assistant",
+            "content": f"{bot_response}"
+            })
+
+            # send a response
+            return bot_response
+            # return output for model
+        except:
+            # print(response.choices[0].text)
+            return "Heyy, i'm facing a little downtime now please try in a few mins"
+            
